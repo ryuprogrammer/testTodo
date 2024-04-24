f4bece7fe3b2d75439240eff39634fa28c94ef23
トム・クルーズ <ryu1421magic@icloud.com>
2024-04-25 01:22:30 +0900

    update notifier

--- testtodo/lib/business/notifire.dart
+++ testtodo/lib/business/notifire.dart
@@ -1,7 +1,33 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 // AsyncNotifierを使用するためにAsyncNotifierProviderを定義
-// final todoController = AsyncNoti
+final todoController =
+    AsyncNotifierProvider<TodoController, void>(TodoController.new);
+
+// Supabaseを操作するAsyncNotifier
+class TodoController extends AsyncNotifier<void> {
+  @override
+  FutureOr<void> build() {}
+
+  // データを追加するメソッド
+  Future<void> addNotes(String _body) async {
+    final appRepository = ref.read(todoController);
+    await appRepository.from('notes').insert({'note_item': _body});
+  }
+
+  // データを更新するメソッド
+  Future<void> updateNotes(dynamic noteID, String _body) async {
+    final appRepository = ref.read(todoController);
+    await appRepository
+        .from('notes')
+        .update({'note_item': _body}).match({'id': noteID});
+  }
+
+  // データを削除するメソッド
+  Future<void> deleteNotes(dynamic noteID) async {
+    final appRepository = ref.read(todoController);
+    await appRepository.from('notes').delete().match({'id': noteID});
+  }
+}
