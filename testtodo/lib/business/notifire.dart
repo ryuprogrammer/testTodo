925586ca4efb49e088d189ee4376d7d041b1254f
トム・クルーズ <ryu1421magic@icloud.com>
2024-04-25 14:31:59 +0900

    fix notifire error

--- testtodo/lib/business/notifire.dart
+++ testtodo/lib/business/notifire.dart
@@ -1,35 +1,49 @@
-import 'dart:async';
-import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-// AsyncNotifierを使用するためにAsyncNotifierProviderを定義
-final todoController =
-    AsyncNotifierProvider<TodoController, void>(TodoController.new);
+class NotesController
+    extends StateNotifier<AsyncValue<List<Map<String, dynamic>>>> {
+  final SupabaseClient _supabaseClient;
 
-final todoStream = Supabase.instance.client;
-
-// Supabaseを操作するAsyncNotifier
-class TodoController extends AsyncNotifier<void> {
-  @override
-  FutureOr<void> build() {}
+  NotesController(this._supabaseClient) : super(AsyncLoading());
 
   // データを追加するメソッド
-  Future<void> addNotes(String _body) async {
-    final appRepository = ref.read(todoController);
-    await appRepository.from('notes').insert({'note_item': _body});
+  Future<void> addNote(String note) async {
+    try {
+      final response =
+          await _supabaseClient.from('todos').insert({'note': note});
+      if (response.error != null) {
+        throw response.error!;
+      }
+    } catch (error) {
+      throw error.toString();
+    }
   }
 
   // データを更新するメソッド
-  Future<void> updateNotes(dynamic noteID, String _body) async {
-    final appRepository = ref.read(todoController);
-    await appRepository
-        .from('notes')
-        .update({'note_item': _body}).match({'id': noteID});
+  Future<void> updateNote(int id, String updatedNote) async {
+    try {
+      final response = await _supabaseClient
+          .from('todos')
+          .update({'note': updatedNote}).eq('id', id);
+      if (response.error != null) {
+        throw response.error!;
+      }
+    } catch (error) {
+      throw error.toString();
+    }
   }
 
   // データを削除するメソッド
-  Future<void> deleteNotes(dynamic noteID) async {
-    final appRepository = ref.read(todoController);
-    await appRepository.from('notes').delete().match({'id': noteID});
+  Future<void> deleteNote(int id) async {
+    try {
+      final response =
+          await _supabaseClient.from('todos').delete().eq('id', id);
+      if (response.error != null) {
+        throw response.error!;
+      }
+    } catch (error) {
+      throw error.toString();
+    }
   }
 }
