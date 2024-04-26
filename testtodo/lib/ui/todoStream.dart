d6c5aedf744ee970e8469a95f7c5d4804515e970
トム・クルーズ <ryu1421magic@icloud.com>
2024-04-26 19:23:55 +0900

    change ui

---
+++ testtodo/lib/ui/todoStream.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class TodoStream extends ConsumerWidget {
+  const TodoStream({super.key})
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: StreamBuilder<List<Map<String, dynamic>>>(
+        stream: _todoStream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final todos = snapshot.data!;
+
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(todos[index]['todo_item']),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: ((context) {
+              return SimpleDialog(
+                title: const Text('新規作成'),
+                contentPadding: const EdgeInsets.all(10),
+                children: [
+                  TextFormField(
+                    onFieldSubmitted: (value) async {
+                      // ここでSupabaseにデータ追加
+                      await Supabase.instance.client
+                          .from('todos')
+                          .insert({'todo_item': value});
+                    },
+                  ),
+                ],
+              );
+            }),
+          );
+        },
+        child: Text(
+          'プラス',
+          style: TextStyle(fontSize: 16),
+        ),
+      ),
+    );
+  }
+}
