018e1489cee22d75fe008ecd2101ae19ecbe6406
トム・クルーズ <ryu1421magic@icloud.com>
2024-04-28 18:15:00 +0900

    add comment

--- testtodo/lib/ui/todoStream.dart
+++ testtodo/lib/ui/todoStream.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+// このファイルがエラーになる
 
 class TodoStream extends ConsumerWidget {
   const TodoStream({super.key})
